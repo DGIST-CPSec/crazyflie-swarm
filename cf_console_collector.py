8a37f9dda335cbb8f43640f0432a1c0415986357
rkdals03 <dev_cpp@kakao.com>
2023-08-22 15:09:45 +0900

    adding call trackers and testing in progress

--- cf_console_collector.py
+++ cf_console_collector.py
@@ -1,5 +1,5 @@
 # collect console printf strings from crazyflie.console
-import time
+import time, datetime, os
 # from threading import Event
 from cflib.crazyflie import Crazyflie
 # from cflib.crazyflie.console import Console as cf_console
@@ -9,26 +9,28 @@ import cflib.crtp
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crtp import pcap as crtp_pcap
 
-URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E702')
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E70A')
+execdate = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+logat = os.path.join('./collected', execdate+'.txt')
 
 class ConsoleCollector:
     def __init__(self, URI):
-        self.logf = open('console_collector.log', 'w')
+        self.logf = open(logat, 'w')
         self.scf = SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache'))
-        self.scf.open_link()
-        self._cf = self.scf.cf
-        self._cf.param.request_update_of_all_params()
-        # self._cf.open_link(URI)
         print('[INFO]: Connecting to %s' % URI)
-        self.initialize(self.scf)
+        self.scf.open_link()
+        print('Connected!')
+        self._cf = self.scf.cf
         self._cf.console.receivedChar.add_callback(self._log_console)
+        # self._cf.open_link(URI)
+        # self.initialize(self.scf)
 
-    def _log_console(self, text):
-        # print(text, end='')
-        self.logf.write(text)
+    def _log_console(self, txt):
+        self.logf.write(txt)
 
     def initialize(self, scf):
         print('initializing...')
+        scf.param.request_update_of_all_params()
         scf.cf.param.set_value('kalman.resetEstimation', '1')
         time.sleep(0.1)
         scf.cf.param.set_value('kalman.resetEstimation', '0')
@@ -39,14 +41,14 @@ class ConsoleCollector:
 
     def terminate(self):
         self.scf.close_link()
+        self.logf.close()
+        print('[INFO]: Console log terminated')
 
 
 
 if __name__ == '__main__':
     # print('whatever just started')
-    cflib.crtp.init_drivers()
-    # print('[INFO]: Connecting to %s' % URI)
-    # with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+    cflib.crtp.init_drivers() 
     cc = ConsoleCollector(URI)
     print('[INFO]: Starting console log')
     time.sleep(10)
